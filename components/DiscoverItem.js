3a37096c4cb3a1895c66bfad05d46d1c9dcd46ed
Volodymyr <klymenkodp@gmail.com>
2019-05-23 21:46:26 -0400

    Move workoutCard to the DiscoverItem

--- components/DiscoverItem.js
+++ components/DiscoverItem.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import WorkoutCard from "../components/WorkoutCard";
 import { View, Text, StyleSheet } from "react-native";
 
 class DiscoverItem extends Component {
   render() {
     return (
-      <>
+      <WorkoutCard>
         <Text style={styles.header}>{this.props.workout.name}</Text>
         <View style={styles.info}>
           <Text>Category: {this.props.workout.category}</Text>
@@ -12,7 +13,7 @@ class DiscoverItem extends Component {
           <Text>Exercises: {this.props.workout.exercises}</Text>
           <Text>Time: {this.props.workout.time}</Text>
         </View>
-      </>
+      </WorkoutCard>
     );
   }
 }
