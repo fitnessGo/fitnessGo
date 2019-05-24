96388758688dc4f5a134849db7a6b3cb946bf4ca
Volodymyr <klymenkodp@gmail.com>
2019-05-23 23:37:52 -0400

    Show popUp on long press

--- components/DiscoverItem.js
+++ components/DiscoverItem.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
 import WorkoutCard from "../components/WorkoutCard";
-import { View, Text, StyleSheet } from "react-native";
+import PopUp from "./PopUp";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 class DiscoverItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { selected: false };
+    this._onPress = this._onPress.bind(this);
+    this._onLongPress = this._onLongPress.bind(this);
+  }
+
+  _onLongPress() {
+    this.setState({ selected: true });
+  }
+
+  _onPress(){
+    this.setState({ selected: false });
+  }
+
   render() {
     return (
       <WorkoutCard>
-        <Text style={styles.header}>{this.props.workout.name}</Text>
-        <View style={styles.info}>
-          <Text>Category: {this.props.workout.category}</Text>
-          <Text>Exercise Count: {this.props.workout.count}</Text>
-          <Text>Exercises: {this.props.workout.exercises}</Text>
-          <Text>Time: {this.props.workout.time}</Text>
-        </View>
+        {this.state.selected && <PopUp />}
+        <TouchableOpacity onLongPress={this._onLongPress} onPress={this._onPress}>
+          <Text style={styles.header}>{this.props.workout.name}</Text>
+          <View style={styles.info}>
+            <Text>Category: {this.props.workout.category}</Text>
+            <Text>Exercise Count: {this.props.workout.count}</Text>
+            <Text>Exercises: {this.props.workout.exercises}</Text>
+            <Text>Time: {this.props.workout.time}</Text>
+          </View>
+        </TouchableOpacity>
       </WorkoutCard>
     );
   }
@@ -20,8 +39,8 @@ class DiscoverItem extends Component {
 
 const styles = StyleSheet.create({
   info: {
-      marginLeft: 10,
-      marginTop: 5
+    marginLeft: 10,
+    marginTop: 5
   },
   header: {
     fontWeight: "bold",
