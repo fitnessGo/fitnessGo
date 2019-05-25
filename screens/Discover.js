03bbf60c784cdcef8e7d8b6ab6a7f59686515a47
Volodymyr <klymenkodp@gmail.com>
2019-05-25 15:16:26 -0400

    Add menuprovider

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,97 +1,100 @@
 import React, { Component } from "react";
 import DiscoverItem from "../components/DiscoverItem";
+import { MenuProvider } from "react-native-popup-menu";
 import { ScrollView, Text, StyleSheet } from "react-native";
 
 class Discover extends Component {
   render() {
     return (
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.content}
-        showsVerticalScrollIndicator="false"
-      >
-        <DiscoverItem
-          workout={{
-            name: "Workout 1",
-            category: "Cardio",
-            count: 2,
-            exercises: "Running, Cycling",
-            time: "30:00"
-          }}
-        />
-        <DiscoverItem
-          workout={{
-            name: "Workout 2",
-            category: "Cardio",
-            count: 2,
-            exercises: "Running, Cycling",
-            time: "30:00"
-          }}
-        />
-        <DiscoverItem
-          workout={{
-            name: "Workout 3",
-            category: "Cardio",
-            count: 2,
-            exercises: "Running, Cycling",
-            time: "30:00"
-          }}
-        />
-        <DiscoverItem
-          workout={{
-            name: "Workout 4",
-            category: "Cardio",
-            count: 2,
-            exercises: "Running, Cycling",
-            time: "30:00"
-          }}
-        />
-        <DiscoverItem
-          workout={{
-            name: "Workout 5",
-            category: "Cardio",
-            count: 2,
-            exercises: "Running, Cycling",
-            time: "30:00"
-          }}
-        />
-        <DiscoverItem
-          workout={{
-            name: "Workout 6",
-            category: "Cardio",
-            count: 2,
-            exercises: "Running, Cycling",
-            time: "30:00"
-          }}
-        />
-        <DiscoverItem
-          workout={{
-            name: "Workout 7",
-            category: "Cardio",
-            count: 2,
-            exercises: "Running, Cycling",
-            time: "30:00"
-          }}
-        />
-        <DiscoverItem
-          workout={{
-            name: "Workout 8",
-            category: "Cardio",
-            count: 2,
-            exercises: "Running, Cycling",
-            time: "30:00"
-          }}
-        />
-        <DiscoverItem
-          workout={{
-            name: "Workout 9",
-            category: "Cardio",
-            count: 2,
-            exercises: "Running, Cycling",
-            time: "30:00"
-          }}
-        />
-      </ScrollView>
+      <MenuProvider>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.content}
+          showsVerticalScrollIndicator="false"
+        >
+          <DiscoverItem
+            workout={{
+              name: "Workout 1",
+              category: "Cardio",
+              count: 2,
+              exercises: "Running, Cycling",
+              time: "30:00"
+            }}
+          />
+          <DiscoverItem
+            workout={{
+              name: "Workout 2",
+              category: "Cardio",
+              count: 2,
+              exercises: "Running, Cycling",
+              time: "30:00"
+            }}
+          />
+          <DiscoverItem
+            workout={{
+              name: "Workout 3",
+              category: "Cardio",
+              count: 2,
+              exercises: "Running, Cycling",
+              time: "30:00"
+            }}
+          />
+          <DiscoverItem
+            workout={{
+              name: "Workout 4",
+              category: "Cardio",
+              count: 2,
+              exercises: "Running, Cycling",
+              time: "30:00"
+            }}
+          />
+          <DiscoverItem
+            workout={{
+              name: "Workout 5",
+              category: "Cardio",
+              count: 2,
+              exercises: "Running, Cycling",
+              time: "30:00"
+            }}
+          />
+          <DiscoverItem
+            workout={{
+              name: "Workout 6",
+              category: "Cardio",
+              count: 2,
+              exercises: "Running, Cycling",
+              time: "30:00"
+            }}
+          />
+          <DiscoverItem
+            workout={{
+              name: "Workout 7",
+              category: "Cardio",
+              count: 2,
+              exercises: "Running, Cycling",
+              time: "30:00"
+            }}
+          />
+          <DiscoverItem
+            workout={{
+              name: "Workout 8",
+              category: "Cardio",
+              count: 2,
+              exercises: "Running, Cycling",
+              time: "30:00"
+            }}
+          />
+          <DiscoverItem
+            workout={{
+              name: "Workout 9",
+              category: "Cardio",
+              count: 2,
+              exercises: "Running, Cycling",
+              time: "30:00"
+            }}
+          />
+        </ScrollView>
+      </MenuProvider>
     );
   }
 }
