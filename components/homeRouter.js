d7b97f39acee0bb29f3d82f2cc4aae4774e6ccd1
Alexei <esqlimite@HOTMAIL.COM>
2019-06-02 18:41:32 -0400

    added route to workout details to app stack

--- components/homeRouter.js
+++ components/homeRouter.js
@@ -5,6 +5,7 @@ import SignInScreen from "../screens/SignIn";
 import CreateAccount from "../screens/CreateAccount";
 import StartScreen from "../screens/Start";
 import HomeScreen from "../screens/Home"
+import WorkoutDetails from "../screens/WorkoutDetails"
 
 //More about navigation https://reactnavigation.org/docs/en/auth-flow.html
 //createStackNavigator is a function that takes a route configuration object and an options object and returns a React component.
@@ -14,6 +15,12 @@ const AppStack = createStackNavigator(
       screen: HomeScreen,
       navigationOptions: {
         title: "Home"
+      }
+    },
+    WorkoutDetails: {
+      screen: WorkoutDetails,
+      navigationOptions: {
+        title: "Details"
       }
     }
   });
