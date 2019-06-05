234a9c25301881ed03636b238aa70199d1cb12bc
Alexei <esqlimite@HOTMAIL.COM>
2019-06-05 15:01:10 -0400

    Added details screen to stack

--- components/homeRouter.js
+++ components/homeRouter.js
@@ -1,10 +1,11 @@
 import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
 
 //Import screens
-import LogInOptionsScreen from "../screens/LogInOptions";
+import SignInScreen from "../screens/LogInOptions";
 import CreateAccount from "../screens/CreateAccount";
 import StartScreen from "../screens/Start";
 import HomeScreen from "../screens/Home"
+import WorkoutDetails from "../screens/WorkoutDetails"
 
 //More about navigation https://reactnavigation.org/docs/en/auth-flow.html
 //createStackNavigator is a function that takes a route configuration object and an options object and returns a React component.
@@ -15,9 +16,15 @@ const AppStack = createStackNavigator(
       navigationOptions: {
         title: "Home"
       }
+    },
+    WorkoutDetails: {
+      screen: WorkoutDetails,
+      navigationOptions: {
+        title: "Details"
+      }
     }
   });
-const AuthStack = createStackNavigator({ SignIn: LogInOptionsScreen, Register: CreateAccount });
+const AuthStack = createStackNavigator({ SignIn: SignInScreen, Register: CreateAccount });
 
 export default createAppContainer(
   createSwitchNavigator(
@@ -27,6 +34,6 @@ export default createAppContainer(
     Auth: AuthStack
   },
   {
-    initialRouteName: 'App',
+    initialRouteName: 'Start',
   })
 );
