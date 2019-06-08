d049d99436799416c4c56b1dcad0ad55be57cacf
Volodymyr <klymenkodp@gmail.com>
2019-06-07 23:40:07 -0400

    Add log out

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,68 +1,79 @@
-import React, { Component } from 'react';
-import {
-    StyleSheet,
-    Button,
-    View,
-    Alert,
-    SafeAreaView
-} from 'react-native';
-import { FontStyles, ScreenStyles } from '../styles/global';
+import React, { Component } from "react";
+import { StyleSheet, Button, View, Alert, SafeAreaView } from "react-native";
+import { FontStyles, ScreenStyles } from "../styles/global";
 import getStyleSheet from "../styles/themestyles";
+import { handleFbLogin, handleFbLogout } from "../lib/auth";
 
 export default class SettingsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      darkTheme: false,
+      dataReady: true
+    };
+    this.deleteAccount = this.deleteAccount.bind(this);
+    this.logout = this.logout.bind(this);
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            darkTheme: false,
-            dataReady: true
+  static navigationOptions = {
+    title: "Settings"
+  };
+
+  //On clicking the Delete Account Button a confirmation box will apear which will ask the user
+  //to cconfirm if they want to delete their account from the system
+  deleteAccount() {
+    Alert.alert(
+      "Confirmation",
+      "Are you sure you want to delete your account?",
+      [
+        //FIXME: On pressing delete the delete account functionality should be implemented
+        { text: "Delete", onPress: () => console.warn("Delete Pressed") },
+        {
+          text: "Cancel",
+          onPress: () => console.warn("Cancel Pressed"),
+          style: "cancel"
         }
-    }
+      ]
+    );
+  }
 
-    static navigationOptions = {
-        title: 'Settings',
-      };
+  logout() {
+    handleFbLogout().then(() => {
+      this.props.navigation.navigate("Auth");
+    })
+    .catch(err => {
+      alert("Couldn't log out. Try again later 🙁");
+    });
+  }
 
-      //On clicking the Delete Account Button a confirmation box will apear which will ask the user
-      //to cconfirm if they want to delete their account from the system
-      onDeleteAccountButtonClick() {
-        Alert.alert(
-            'Confirmation',
-            'Are you sure you want to delete your account?',
-            [
-                //FIXME: On pressing delete the delete account functionality should be implemented
-              {text: 'Delete', onPress: () => console.warn('Delete Pressed')},
-              {text: 'Cancel', onPress: () => console.warn('Cancel Pressed'), style: 'cancel'},
-            ]
-          );
-      }
-    
-    render() {
-        const theme = getStyleSheet(this.state.darkTheme); 
-        const workoutViewStyle = this.state.darkTheme ? styles.workoutViewDark: styles.workoutViewLight
-        return (
-            <SafeAreaView  style={[ScreenStyles.screenContainer, theme.background]}>
-                <View style={styles.container}>
-                    <Button
-                        buttonStyle={styles.button}
-                        title="Delete Account"
-                        onPress={() => {
-                            this.onDeleteAccountButtonClick();
-                        }}
-                    ></Button>
-                </View>
-            </SafeAreaView>
-        );
-    }
+  render() {
+    const theme = getStyleSheet(this.state.darkTheme);
+    const workoutViewStyle = this.state.darkTheme
+      ? styles.workoutViewDark
+      : styles.workoutViewLight;
+    return (
+      <SafeAreaView style={[ScreenStyles.screenContainer, theme.background]}>
+        <View style={styles.container}>
+          <Button
+            style={styles.button}
+            title="Delete Account"
+            color="red"
+            onPress={this.deleteAccount}
+          />
+          <Button style={styles.button} title="Logout" onPress={this.logout} />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    button: {
-        marginTop: '15%' 
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    marginTop: "15%"
+  }
 });
