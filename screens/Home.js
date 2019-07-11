77b9c20eeb812fc187294be554e6cf68d4a560ed
Alexei <esqlimite@HOTMAIL.COM>
2019-07-11 17:43:57 -0400

    Fix crash

--- screens/Home.js
+++ screens/Home.js
@@ -62,7 +62,7 @@ class HomeScreen extends React.Component {
             this.setState({ workouts });
         });
     } else {
-      Alert.alert("Couldn't fetch your workouts 😔 Try again later.", message);
+      Alert.alert("Couldn't fetch your workouts 😔 Try again later.");
     }
   }
 
