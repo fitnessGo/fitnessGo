07898abb1bfb7aa3cf2ddae9fc814b38c4ffa37c
Volodymyr <klymenkodp@gmail.com>
2019-07-13 20:16:08 -0400

    feature: delete workout functionality

--- screens/Home.js
+++ screens/Home.js
@@ -3,11 +3,21 @@ import {
   ScrollView,
   View,
   StyleSheet,
-  Text, Alert,
-  SafeAreaView, RefreshControl
+  Text,
+  Alert,
+  SafeAreaView,
+  RefreshControl,
+  TouchableOpacity
 } from "react-native";
 import { Button, Icon } from "react-native-elements";
 import WorkoutView from "../components/WorkoutInfoView";
+import {
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+  MenuProvider
+} from "react-native-popup-menu";
 import getStyleSheet from "../styles/themestyles";
 import { ScreenStyles } from "../styles/global";
 import firebase from "react-native-firebase";
@@ -51,24 +61,28 @@ class HomeScreen extends React.Component {
   }
 
   componentDidMount() {
-    this.fetchUserWorkouts((workouts) => {
+    this.fetchUserWorkouts(workouts => {
       this.setState({ workouts: workouts });
-    })
+    });
   }
 
   fetchUserWorkouts(onCompletion) {
     const user = firebase.auth().currentUser;
     if (user) {
-      firebase.database().ref("users/" + user.uid + "/workouts").on('value', (snapshot) => {
-        let workouts = [];
-        snapshot.forEach(snap => {
-          workouts.push(snap.val());
+      firebase
+        .database()
+        .ref("users/" + user.uid + "/workouts")
+        .on("value", snapshot => {
+          let workouts = [];
+          snapshot.forEach(snap => {
+            let workout = { id: snap.key, ...snap.val() };
+            workouts.push(workout);
+          });
+          onCompletion(workouts);
         });
-        onCompletion(workouts)
-      });
     } else {
       Alert.alert("Couldn't fetch your workouts 😔 Try again later.");
-      onCompletion(null)
+      onCompletion(null);
     }
   }
   componentWillUnmount() {
@@ -98,10 +112,16 @@ class HomeScreen extends React.Component {
   }
   _onRefresh = () => {
     this.setState({ refreshing: true });
-    this.fetchUserWorkouts((workouts) => {
+    this.fetchUserWorkouts(workouts => {
       this.setState({ refreshing: false, workouts: workouts });
     });
+  };
+
+  deleteWorkout(workout) {
+    const user = firebase.auth().currentUser;
+    firebase.database().ref("users/" + user.uid + "/workouts").child(workout.id).remove();
   }
+
   render() {
     const theme = getStyleSheet(this.state.darkTheme);
     const workoutViewStyle = this.state.darkTheme
@@ -111,8 +131,14 @@ class HomeScreen extends React.Component {
       return (
         <SafeAreaView style={[ScreenStyles.screenContainer, theme.background]}>
           <ScrollView
-            style={[ScreenStyles.screenContainer, styles.workoutViewContainer]} showsVerticalScrollIndicator={false}
-            refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={this._onRefresh} />}
+            style={[ScreenStyles.screenContainer, styles.workoutViewContainer]}
+            showsVerticalScrollIndicator={false}
+            refreshControl={
+              <RefreshControl
+                refreshing={this.state.refreshing}
+                onRefresh={this._onRefresh}
+              />
+            }
           >
             <View style={{ flex: 1, alignItems: "center" }}>
               <Text style={theme.text}>
@@ -132,35 +158,66 @@ class HomeScreen extends React.Component {
     return (
       // var workoutViews = new Array();
       <SafeAreaView style={[ScreenStyles.screenContainer, theme.background]}>
-        <ScrollView style={ScreenStyles.screenContainer} showsVerticalScrollIndicator={false}
-            refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={this._onRefresh} />}>
-          <View ref="workoutsView" style={styles.workoutViewContainer}>
-            {this.state.workouts.map((w, index) => {
-              return (
-                <WorkoutView
-                  key={index}
-                  style={workoutViewStyle}
-                  workout={w}
-                  onPress={(workout, view) =>
-                    this._onWorkoutSelect(workout, view)
-                  }
-                  onPlayButtonClick={workout =>
-                    this._onPlayButtonClick(workout)
-                  }
-                />
-              );
-            })}
-          </View>
-        </ScrollView>
-        <View style={{ alignItems: "flex-end" }}>
-          <Button
-            type="clear"
-            icon={<Icon name="add-circle" size={44} color={theme.text.color} />}
-            onPress={workout =>
-              this._onCreateNewButtonClick(this.state.workouts)
+        <MenuProvider>
+          <ScrollView
+            style={ScreenStyles.screenContainer}
+            showsVerticalScrollIndicator={false}
+            refreshControl={
+              <RefreshControl
+                refreshing={this.state.refreshing}
+                onRefresh={this._onRefresh}
+              />
             }
-          />
-        </View>
+          >
+            <View ref="workoutsView" style={styles.workoutViewContainer}>
+              {this.state.workouts.map((w, index) => {
+                return (
+                  <Menu key={index}>
+                    <MenuTrigger
+                      triggerOnLongPress={true}
+                      customStyles={triggerMenuTouchable}
+                      onAlternativeAction={view =>
+                        this._onWorkoutSelect(w, view)
+                      }
+                    >
+                      <WorkoutView
+                        style={workoutViewStyle}
+                        workout={w}
+                        onPress={(workout, view) =>
+                          this._onWorkoutSelect(workout, view)
+                        }
+                        onPlayButtonClick={workout =>
+                          this._onPlayButtonClick(workout)
+                        }
+                      />
+                    </MenuTrigger>
+                    <MenuOptions customStyles={popUpStyles}>
+                      <MenuOption
+                        text="Details"
+                        onSelect={view => this._onWorkoutSelect(w, view)}
+                      />
+                      <MenuOption
+                        text="Delete"
+                        onSelect={() => this.deleteWorkout(w)}
+                      />
+                    </MenuOptions>
+                  </Menu>
+                );
+              })}
+            </View>
+          </ScrollView>
+          <View style={{ alignItems: "flex-end" }}>
+            <Button
+              type="clear"
+              icon={
+                <Icon name="add-circle" size={44} color={theme.text.color} />
+              }
+              onPress={workout =>
+                this._onCreateNewButtonClick(this.state.workouts)
+              }
+            />
+          </View>
+        </MenuProvider>
       </SafeAreaView>
     );
   }
@@ -186,5 +243,14 @@ const styles = StyleSheet.create({
     marginBottom: 10
   }
 });
+
+const popUpStyles = {
+  optionsContainer: {
+    borderRadius: 6,
+    width: 130
+  }
+};
+
+const triggerMenuTouchable = { TriggerTouchableComponent: TouchableOpacity };
 
 export default HomeScreen;
