4b2369b4d34299e6f04610aa7ec742c9fa9fa9ff
Volodymyr <klymenkodp@gmail.com>
2019-07-30 12:24:48 -0400

    Add images

--- screens/ExerciseDetailsView.js
+++ screens/ExerciseDetailsView.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
 import Card from '../components/Card';
 import { FontStyles } from '../styles/global';
 import SetDetailsView from './SetDetailsView';
+import { ExerciseImages } from '../components/ExerciseImages';
+
 
 class ExerciseDetailsView extends React.Component {
     render() {
         const exerciseViewStyle = this.props.darkTheme || false ? exerciseViewStyles.exersiseViewDark : exerciseViewStyles.exersiseViewLight
         const exerciseViewTextStyle = this.props.darkTheme || false ? exerciseViewStyles.exersiseViewTextDark : exerciseViewStyles.exersiseViewTextLight
+
+        let image;
+        if (ExerciseImages[this.props.exercise.name]) {
+            image =
+                <Image
+                    resizeMode={'contain'}
+                    style={ styles.imageStyle }
+                    source={ExerciseImages[this.props.exercise.name]}
+                />
+        }
+
         return (
             <View style={this.props.style}>
                 <Card style={exerciseViewStyle}>
                     <Text style={[exerciseViewTextStyle, { ...FontStyles.h1, ...FontStyles.bold }]}>{this.props.exercise.name}</Text>
                     <Text style={exerciseViewTextStyle}>{this.props.exercise.description}</Text>
+                    <View style={{ width: '100%',  alignItems: 'center'  }}>
+
+                    {image && image}
+                    </View>
                     <View style={{ marginTop: 10 }}>
                         {this.props.exercise.exerciseSets.map((es, index) => {
                             return (
@@ -48,5 +65,12 @@ const exerciseViewStyles = StyleSheet.create({
         fontSize: 16
     }
 });
+
+const styles = StyleSheet.create({
+    imageStyle: {
+      height: 80,
+      resizeMode: 'contain'
+    }
+  });
 
 export default ExerciseDetailsView;
